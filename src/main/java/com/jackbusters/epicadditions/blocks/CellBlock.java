85f43de0fd3a0e67242f656c7f8076058639aca5
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-08-22 12:23:28 -0500

    Fixed Ender Pearls being used to escape cell

--- src/main/java/com/jackbusters/epicadditions/blocks/CellBlock.java
+++ src/main/java/com/jackbusters/epicadditions/blocks/CellBlock.java
@@ -1,7 +1,11 @@
 package com.jackbusters.epicadditions.blocks;
 
+import com.jackbusters.epicadditions.capabilities.pocketcells.PocketCellLevelDataProvider;
+import com.jackbusters.epicadditions.capabilities.pocketcells.PocketCellProvider;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.HalfTransparentBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -27,5 +31,16 @@ public class CellBlock extends HalfTransparentBlock {
     @Override
     public boolean propagatesSkylightDown(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos) {
         return true;
+    }
+
+    @Override
+    public void entityInside(@NotNull BlockState pState, @NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull Entity pEntity) {
+        pEntity.getCapability(PocketCellProvider.POCKET_CELL_DATA).ifPresent(data -> {
+            pLevel.getCapability(PocketCellLevelDataProvider.POCKET_CELL_LEVEL_DATA).ifPresent(lData -> {
+               BlockPos pos = lData.getTangibleCellLocations().get(data.getPocketCellIndex());
+               pEntity.teleportTo(pos.getX(), pos.getY()+1, pos.getZ());
+            });
+        });
+        super.entityInside(pState, pLevel, pPos, pEntity);
     }
 }
