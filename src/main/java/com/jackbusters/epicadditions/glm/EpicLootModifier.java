d0230984f5fb715d81cf06da1b5c567feb25f240
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-08-23 18:26:54 -0500

    Added wither drop

--- src/main/java/com/jackbusters/epicadditions/glm/EpicLootModifier.java
+++ src/main/java/com/jackbusters/epicadditions/glm/EpicLootModifier.java
@@ -5,9 +5,12 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
@@ -24,7 +27,7 @@ public class EpicLootModifier extends LootModifier {
     public static final Supplier<Codec<EpicLootModifier>> CODEC = Suppliers.memoize(() ->
             RecordCodecBuilder.create(instance -> codecStart(instance).and(ForgeRegistries.ITEMS.getCodec()
                     .fieldOf("item").forGetter(a -> a.item)).and(Codec.INT
-                    .fieldOf("amount").forGetter(b -> b.amount)).and(Codec.STRING.fieldOf("loot_table")
+                    .fieldOf("amount").forGetter(b -> b.amount)).and(Codec.STRING
                     .fieldOf("loot_table").forGetter(c -> c.lootTable)).apply(instance, EpicLootModifier::new)));
 
     private final Item item;
