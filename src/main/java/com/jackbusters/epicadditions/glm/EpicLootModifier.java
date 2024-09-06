5bf2fec1aa422f2ce6c518d3c2d4bc57eb83e57a
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-09-06 00:40:22 -0500

    Added chance to glm

--- src/main/java/com/jackbusters/epicadditions/glm/EpicLootModifier.java
+++ src/main/java/com/jackbusters/epicadditions/glm/EpicLootModifier.java
@@ -25,23 +25,34 @@ public class EpicLootModifier extends LootModifier {
             RecordCodecBuilder.create(instance -> codecStart(instance).and(ForgeRegistries.ITEMS.getCodec()
                     .fieldOf("item").forGetter(a -> a.item)).and(Codec.INT
                     .fieldOf("amount").forGetter(b -> b.amount)).and(Codec.STRING
-                    .fieldOf("loot_table").forGetter(c -> c.lootTable)).apply(instance, EpicLootModifier::new)));
+                    .fieldOf("loot_table").forGetter(c -> c.lootTable)).and(Codec.DOUBLE
+                    .fieldOf("chance").forGetter(d -> d.chance))
+                    .apply(instance, EpicLootModifier::new)));
 
-    private final Item item;
-    private final int amount;
-    private final String lootTable;
+    private final Item item; // The item to generate
+    private final int amount; // the number of items
+    private final String lootTable; // the loot table to generate the loot in
+    private final double chance; // The chance of the loot generating 0 being no chance, 1 being guaranteed.
 
-    protected EpicLootModifier(LootItemCondition[] conditionsIn, Item item, int amount, String lootTable) {
+    protected EpicLootModifier(LootItemCondition[] conditionsIn, Item item, int amount, String lootTable, double chance) {
         super(conditionsIn);
         this.item = item;
         this.amount = amount;
         this.lootTable = lootTable;
+        this.chance = chance;
     }
 
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> objectArrayList, LootContext lootContext) {
-        if(lootContext.getQueriedLootTableId().equals(ResourceLocation.tryParse(lootTable)))
-            objectArrayList.add(new ItemStack(item, amount));
+        if(lootContext.getQueriedLootTableId().equals(ResourceLocation.tryParse(lootTable))) {
+            if(chance == 1)
+                objectArrayList.add(new ItemStack(item, amount));
+            else{
+                double random = Math.random();
+                if(random < chance)
+                    objectArrayList.add(new ItemStack(item, amount));
+            }
+        }
         return objectArrayList;
     }
 
