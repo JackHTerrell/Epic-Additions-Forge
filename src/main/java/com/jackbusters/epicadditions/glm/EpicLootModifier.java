36214d8906a96e0260f3040b009c00beb5cc94dc
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-09-06 01:42:43 -0500

    Soul Tied argument added

--- src/main/java/com/jackbusters/epicadditions/glm/EpicLootModifier.java
+++ src/main/java/com/jackbusters/epicadditions/glm/EpicLootModifier.java
@@ -1,12 +1,16 @@
 package com.jackbusters.epicadditions.glm;
 
 import com.google.common.base.Suppliers;
+import com.jackbusters.epicadditions.EpicRegistry;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
@@ -23,20 +27,23 @@ public class EpicLootModifier extends LootModifier {
 
     public static final Supplier<Codec<EpicLootModifier>> CODEC = Suppliers.memoize(() ->
             RecordCodecBuilder.create(instance -> codecStart(instance).and(ForgeRegistries.ITEMS.getCodec()
-                    .fieldOf("item").forGetter(a -> a.item)).and(Codec.INT
-                    .fieldOf("amount").forGetter(b -> b.amount)).and(Codec.STRING
-                    .fieldOf("loot_table").forGetter(c -> c.lootTable)).and(Codec.DOUBLE
-                    .fieldOf("chance").forGetter(d -> d.chance))
+                    .fieldOf("item").forGetter(a -> a.item)).and(Codec.BOOL
+                    .fieldOf("soul_tied").forGetter(b -> b.soulTied)).and(Codec.INT
+                    .fieldOf("amount").forGetter(c -> c.amount)).and(Codec.STRING
+                    .fieldOf("loot_table").forGetter(d -> d.lootTable)).and(Codec.DOUBLE
+                    .fieldOf("chance").forGetter(e -> e.chance))
                     .apply(instance, EpicLootModifier::new)));
 
     private final Item item; // The item to generate
+    private final boolean soulTied; // If true, loot will be soul tied.
     private final int amount; // the number of items
     private final String lootTable; // the loot table to generate the loot in (i.e. "minecraft:entities/wither").
     private final double chance; // The chance of the loot generating 0 being no chance, 1 being guaranteed.
 
-    protected EpicLootModifier(LootItemCondition[] conditionsIn, Item item, int amount, String lootTable, double chance) {
+    protected EpicLootModifier(LootItemCondition[] conditionsIn, Item item, boolean soulTied, int amount, String lootTable, double chance) {
         super(conditionsIn);
         this.item = item;
+        this.soulTied = soulTied;
         this.amount = amount;
         this.lootTable = lootTable;
         this.chance = chance;
@@ -45,12 +52,14 @@ public class EpicLootModifier extends LootModifier {
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> objectArrayList, LootContext lootContext) {
         if(lootContext.getQueriedLootTableId().equals(ResourceLocation.tryParse(lootTable))) {
+            ItemStack itemStack = getItemStackWithEnchantments();
+
             if(chance == 1)
-                objectArrayList.add(new ItemStack(item, amount));
+                objectArrayList.add(itemStack);
             else{
                 double random = Math.random();
                 if(random < chance)
-                    objectArrayList.add(new ItemStack(item, amount));
+                    objectArrayList.add(itemStack);
             }
         }
         return objectArrayList;
@@ -59,5 +68,19 @@ public class EpicLootModifier extends LootModifier {
     @Override
     public Codec<? extends IGlobalLootModifier> codec() {
         return CODEC.get();
+    }
+
+    /*
+        Just condensing this to a method to improve readability.
+        Will enchant the item normally, unless it is an enchantment book.
+        In which case, will add enchantment for use.
+     */
+    private ItemStack getItemStackWithEnchantments(){
+        ItemStack itemStack = new ItemStack(item, amount);
+        if(soulTied && item != Items.ENCHANTED_BOOK)
+            itemStack.enchant(EpicRegistry.SOUL_TIED.get(), 1);
+        else if(soulTied)
+            EnchantedBookItem.addEnchantment(itemStack, new EnchantmentInstance(EpicRegistry.SOUL_TIED.get(), 1));
+        return itemStack;
     }
 }
