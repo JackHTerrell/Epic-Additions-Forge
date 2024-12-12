8b7a3086897ab1c7ef3a4c767cf6958ca29d89cf
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2024-12-12 04:12:40 -0600

    JavaDoc on Soul Tied Enchantment

--- src/main/java/com/jackbusters/epicadditions/enchantments/SoulTiedEnchantment.java
+++ src/main/java/com/jackbusters/epicadditions/enchantments/SoulTiedEnchantment.java
@@ -1,9 +1,15 @@
 package com.jackbusters.epicadditions.enchantments;
 
+import com.jackbusters.epicadditions.mixins.PlayerDropEquipment;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
+/**
+ * <h1>The Soul Tied Enchantment</h1>
+ * <p>The Soul Tied enchantment prevents enchanted items from being lost upon death.</p>
+ * <p>Most functionality is handled in {@link SoulTiedEnchantmentEvents} and {@link PlayerDropEquipment}.</p>
+ */
 public class SoulTiedEnchantment extends Enchantment {
     public SoulTiedEnchantment(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot[] pApplicableSlots) {
         super(pRarity, pCategory, pApplicableSlots);
