ca28a3f8a726ca07f90b0a80ce586e7a37b4228d
Jackbusters <77648717+JackHTerrell@users.noreply.github.com>
2025-04-12 21:42:47 -0500

    Condensed logic for Soft Stepping

--- src/main/java/com/jackbusters/epicadditions/enchantments/SoftSteppingChecker.java
+++ src/main/java/com/jackbusters/epicadditions/enchantments/SoftSteppingChecker.java
@@ -5,12 +5,19 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.gameevent.GameEvent;
+
+import java.util.List;
 
 /**
  * <h1>Soft Stepping Checker</h1>
  * <p>This class houses a collection of functions for checking soft stepping-related statuses.</p>
  */
 public class SoftSteppingChecker {
+
+    private static final List<GameEvent> SILENCED_GAME_EVENTS =
+            List.of(GameEvent.STEP, GameEvent.HIT_GROUND, GameEvent.PROJECTILE_SHOOT, GameEvent.EQUIP);
+
     /*
         Returns true if passed entity is wearing boots with the soft stepping enchantment.
      */
@@ -23,5 +30,9 @@ public class SoftSteppingChecker {
      */
     public static boolean isItemStackSoftStepper(ItemStack itemStack){
         return EnchantmentHelper.getTagEnchantmentLevel(EpicRegistry.SOFT_STEPPING.get(), itemStack) > 0;
+    }
+
+    public static boolean isSilencedVibration(GameEvent vibration){
+        return SILENCED_GAME_EVENTS.contains(vibration);
     }
 }
--- src/main/java/com/jackbusters/epicadditions/mixins/softstepping/VibrationAcceptorEdits.java
+++ src/main/java/com/jackbusters/epicadditions/mixins/softstepping/VibrationAcceptorEdits.java
@@ -12,35 +12,23 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
+/**
+ * <h1>Vibration Selector Mixin</h1>
+ * <p>Handles the selection of vibrations from anything that detects vibrations (i.e. Warden, Sculk Sensor)</p>
+ */
 @Mixin(VibrationSelector.class)
 public class VibrationAcceptorEdits {
 
+    /*
+        If true, game event from player will be detected as a vibration.
+        If false, the game event from the player will not be detected as a vibration.
+     */
     @Inject(at = @At("HEAD"), method = "shouldReplaceVibration", cancellable = true)
     void canAccept(VibrationInfo vibration, long tick, CallbackInfoReturnable<Boolean> cir) {
             Entity entity = vibration.entity();
             GameEvent event = vibration.gameEvent();
-            if(entity instanceof LivingEntity aliveEntity)
-            {
-                if(SoftSteppingChecker.isWearingSoftSteppers(aliveEntity))
-              {
-                if(event.equals(GameEvent.STEP))
-                {
-                  cir.setReturnValue(false);
-                }
-                if(event.equals(GameEvent.HIT_GROUND))
-                {
-                  cir.setReturnValue(false);
-                }
-                if(event.equals(GameEvent.PROJECTILE_SHOOT))
-                {
-                  cir.setReturnValue(false);
-                }
-                if(event.equals(GameEvent.EQUIP))
-                {
-                  cir.setReturnValue(false);
-                }
-              }
-            }
+            if(entity instanceof LivingEntity aliveEntity && SoftSteppingChecker.isWearingSoftSteppers(aliveEntity))
+                cir.setReturnValue(!SoftSteppingChecker.isSilencedVibration(event));
           }
 }
     
